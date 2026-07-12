75fec62c6c688db380698507d8b274c411cbe3c3
True North Associates <truenorthassociates5@gmail.com>
2026-07-12 15:48:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// TRUE NORTH ASSOCIATES
+
+console.log("Website Loaded Successfully");
+
+// Smooth Scroll
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if(target){
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+// Header Shadow on Scroll
+window.addEventListener('scroll', function(){
+
+    const header = document.querySelector('header');
+
+    if(window.scrollY > 50){
+        header.style.boxShadow = "0 5px 15px rgba(0,0,0,.3)";
+    }else{
+        header.style.boxShadow = "none";
+    }
+
+});
