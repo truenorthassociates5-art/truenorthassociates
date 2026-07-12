b9ef1bde8fd3b693f15e4529d8b79110ffd7f9e6
True North Associates <truenorthassociates5@gmail.com>
2026-07-12 18:45:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,11 +35,4 @@ window.addEventListener('scroll', function(){
 
 function closeImage(){
     document.getElementById("popup").style.display="none";
-}function openImage(src){
-    document.getElementById("popup").style.display="flex";
-    document.getElementById("popup-img").src=src;
-}
-
-function closeImage(){
-    document.getElementById("popup").style.display="none";
 }
